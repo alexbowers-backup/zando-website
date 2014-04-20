2ecd6f580a2fa2655bc9e12792f38cb4bc44f1cf
Alex Bowers <bowersbros@gmail.com>
2014-04-20 01:37:22 +0100

    removed example app key
    Removed example key to make it clear that the key needs to be set by the user

--- .env.local.php
+++ .env.local.php
@@ -1,7 +1,7 @@
 <?php
 	return array(
 		'debug' => true,
-		'secret_key' => 'Rb2X8Jpl3suZm5FFmvyORT1Lvj6t8bK2', ## Change to your own 32 digit key. running and copying php artisan key:generate will give you a random key
+		'secret_key' => '', ## Change to your own 32 digit key. running and copying php artisan key:generate will give you a random key
 		'mysql-host' => 'localhost',
 		'mysql-db' => 'zando',
 		'mysql-user' => 'root',
