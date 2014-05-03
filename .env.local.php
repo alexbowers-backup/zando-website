28957f9cfa413d49f49459a9378d50d1a90c32ed
Alex Bowers <bowersbros@gmail.com>
2014-05-03 19:31:33 +0100

    Update .env.local.php

--- .env.local.php
+++ .env.local.php
@@ -1,10 +1,10 @@
 <?php
 	return array(
 		'debug' => true,
-		'secret_key' => '', ## Change to your own 32 digit key. running and copying php artisan key:generate will give you a random key
+		'secret_key' => '12345678912345678912345678912312', ## Change to your own 32 digit key. running and copying php artisan key:generate will give you a random key
 		'mysql-host' => 'localhost',
-		'mysql-db' => 'zando',
-		'mysql-user' => 'root',
-		'mysql-pass' => 'root',
+		'mysql-db' => 'zando', # zando for travis. Change to be your local version.
+		'mysql-user' => 'root', # root for travis. Change to be your local version
+		'mysql-pass' => '', # blank for travis. Change to be your local version
 		'mysql-prefix' => ''
 	);
